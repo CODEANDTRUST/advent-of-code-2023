eec5f2f3b7bdf544a0d63fd8a600b539f5c17b8e
BlakeIngle <blake.ingle7@gmail.com>
2023-12-08 12:59:43 -0500

    day 8 part 2 - attempt 1

--- 8-Haunted-Wasteland/part-2/blake.ts
+++ 8-Haunted-Wasteland/part-2/blake.ts
@@ -50,27 +50,49 @@ let i = networkNodes.length - 1
 
 let startNodes: Node[] = []
 
-while (i > 0) {
+// returns true if nodes were attached
+function attachNodes(rootNode: Node, otherNode: Node): boolean {
+
+    let attachments = 0
+
+    if (rootNode.left?.name === otherNode?.name) {
+        rootNode.left = otherNode
+        attachments++
+    }
+    if (rootNode.right?.name === otherNode?.name) {
+        rootNode.right = otherNode
+        attachments++
+    }
+    if (otherNode?.left?.name === rootNode.name) {
+        otherNode.left = rootNode
+        attachments++
+    }
+    if (otherNode?.right?.name === rootNode.name) {
+        otherNode.right = rootNode
+        attachments++
+    }
+
+    return attachments > 0
+}
+
+while (i >= 0) {
 
     let looseNode = networkNodes.pop() // end
 
 
     for (let node of networkNodes) {
-        if (node.left?.name === looseNode?.name) {
-            node.left = looseNode
+        if (looseNode !== undefined) {
+            attachNodes(node, looseNode)
         }
-        if (node.right?.name === looseNode?.name) {
-            node.right = looseNode
-        }
-        if (looseNode?.left?.name === node.name) {
-            looseNode.left = node
-        }
-        if (looseNode?.right?.name === node.name) {
-            looseNode.right = node
+    }
+    for (let node of pointerNodes) {
+        if (looseNode !== undefined) {
+            attachNodes(node, looseNode)
         }
     }
 
     if (looseNode?.name.endsWith("A")) {
+        console.log(looseNode.name)
         startNodes.push(looseNode)
     }
 
@@ -79,8 +101,61 @@ while (i > 0) {
 
 console.log({ startNodes })
 
+function countStepsToAllEnds(rootNode: Node) {
+    let stepsTaken = 0
+    let stepsNeeded = 0
 
+    let pointer = rootNode
 
+    // I just hope 1000 times through is enough
+    for (let j = 0; j < 1000; j++) {
+        for (let i = 0; i < rls.length; i++) {
+            let direction = rls[i]
+            if (direction === 'L') {
+                pointer = pointer.left as Node
+                stepsTaken++
+            } else {
+                pointer = pointer.right as Node
+                stepsTaken++
+            }
+            if (pointer.name.endsWith('Z')) {
+                // console.log("End found after", stepsTaken, 'steps')
+                // if (stepsTaken === 2 * stepsNeeded) {
+                //      return stepsNeeded
+                // }
+                // stepsNeeded = stepsTaken
+                return stepsTaken
+            }
+        }
+    }
+    console.log("ERROR, irregular pattern")
+    return stepsTaken
+}
+
+let stepsNeededArr: number[] = []
+nodeLoop: for (let root of pointerNodes) {
+    const stepsToEnd = countStepsToAllEnds(root)
+    // console.log(stepsToEnd)
+
+    for (let i = 0; i < stepsNeededArr.length; i++) {
+        let s = stepsNeededArr[i]
+
+        if ((stepsToEnd % s) === 0) {
+            stepsNeededArr[i] = stepsToEnd
+            continue nodeLoop
+        }
+    }
+    stepsNeededArr.push(stepsToEnd)
+}
+
+console.log({ stepsNeededArr })
+
+let product: bigint = 1n;
+for (let n of stepsNeededArr) {
+    product *= BigInt(n)
+}
+
+console.log({ answer: product })
 
 const t3 = performance.now()
 console.log("Input read time - ", t2 - t1, 'ms')
