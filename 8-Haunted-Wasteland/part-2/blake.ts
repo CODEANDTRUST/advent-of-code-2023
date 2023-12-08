b9d737764865eaf04dd286a0b860922691895f43
BlakeIngle <blake.ingle7@gmail.com>
2023-12-08 14:22:33 -0500

    omg lcm not gcd

--- 8-Haunted-Wasteland/part-2/blake.ts
+++ 8-Haunted-Wasteland/part-2/blake.ts
@@ -85,7 +85,7 @@ while (networkNodes.length > 0) {
 
 }
 
-console.log({ startNodes })
+// console.log({ startNodes })
 
 // find if any of the start nodes share the same tree
 
@@ -100,9 +100,10 @@ function checkNode(node: Node) {
 }
 
 const MAX_STEPS = 10000000
-//
-function countStepsToEnds(root: Node): number[] {
-    let stepsToEnd: number[] = []
+// networks contain 1 start and 1 end
+// following instructions will always take the same number of steps 
+// to land on an end position
+function countStepsToEnds(root: Node): number {
 
     let currentNode = root
     let stepsTaken = 0
@@ -115,37 +116,33 @@ function countStepsToEnds(root: Node): number[] {
 
             if (direction === 'L') {
                 currentNode.left && (currentNode = currentNode.left)
-            } else {
+            } else if (direction === 'R') {
                 currentNode.right && (currentNode = currentNode.right)
+            } else {
+                console.log("BAD INPUT:", direction)
             }
             stepsTaken++
 
             if (currentNode.name.endsWith('Z')) {
-                stepsToEnd.push(stepsTaken)
-                if (stepsTaken % stepsToEnd[0] > 0) {
-                    console.log({ irregular: stepsTaken })
-                }
+                return stepsTaken
             }
         }
 
-        // if you finished walking through the instructions, find out if you end up at the start again
-        if (currentNode.name === root.name) {
-            break;
-        }
         if (stepsTaken >= MAX_STEPS) {
             break
         }
     }
 
-    return stepsToEnd
+    return stepsTaken
 }
+
+let minSteps: number[] = []
 
 for (let root of startNodes) {
     foundNames = []
     checkNode(root) // find names in network
 
-    const ns = countStepsToEnds(root)
-    console.log({ ns })
+    minSteps.push(countStepsToEnds(root))
     // console.log({
     //     rootName: root.name,
     //     names: foundNames.length,
@@ -153,6 +150,63 @@ for (let root of startNodes) {
     //     namesWithZ: foundNames.filter(name => name.endsWith('Z')),
     // })
 }
+
+console.log({ minSteps })
+
+// confirmed the steps needed are not factors of each other
+// for (let s of minSteps) {
+//     for (let s2 of minSteps) {
+//         if (s === s2) {
+//             continue
+//         }
+//         if (s % s2 === 0) {
+//             console.log("found one", { s, s2 })
+//         } else if (s2 % s === 0) {
+//             console.log("found one", { s, s2 })
+//         }
+//     }
+// }
+
+// confirming the values are not bad
+// let p = 1n
+// for (let i = 0; i < minSteps.length; i++) {
+//     let n = minSteps[i]
+//     p *= BigInt(n)
+//     for (let j = i + 1; j < minSteps.length; j++) {
+//         let n2 = minSteps[j]
+//         if (p % BigInt(n2) === 0n) {
+//             console.log("WTF", n2)
+//         }
+//     }
+// }
+// console.log({ p })
+
+let lcm = 1
+
+function getLcm(a, b): number {
+
+    let min = Math.min(a, b)
+    let max = a + b - min
+
+    let i = 1;
+    while (true) {
+        if ((max * i) % min === 0) {
+            return max * i
+        }
+
+        if (++i > 1000000) {
+            break
+        }
+    }
+    console.log("Something is wrong")
+    return 0
+}
+
+for (let n of minSteps) {
+    lcm = getLcm(n, lcm)
+}
+
+console.log({ lcm })
 
 const t3 = performance.now()
 console.log("Input read time - ", t2 - t1, 'ms')
